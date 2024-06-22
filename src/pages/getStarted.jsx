76ae4390e79fd728e1522745bef01a9da5bcc0c6
eghosa <eghosajosh@gmail.com>
2024-06-22 05:01:57 +0100

    login

--- src/pages/getStarted.jsx
+++ src/pages/getStarted.jsx
@@ -1,67 +1,14 @@
 import React, { useEffect, useState } from "react";
 import { motion } from "framer-motion";
-import supabase from "../scripts/supabase";
 import { toast, ToastContainer } from "react-toastify";
-import { getWebString, setWebString } from "../utils/bridge";
 import { useNavigate } from "react-router-dom";
 
 function GetStarted({ setLoggedIn }) {
-  let [previousWebString, setPreviousWebString] = useState("");
   let navigate = useNavigate();
 
-  useEffect(() => {
-    let stringChecker = setInterval(() => {
-      if (getWebString() != null) {
-        if (previousWebString != getWebString()) {
-          setPreviousWebString("");
-          let email = getWebString();
-          if (email.indexOf("@") >= 0) loginUser(email);
-        }
-      }
-    }, 500);
-
-    return () => clearInterval(stringChecker);
-  }, []);
-
-  let getStarted = () => {
-    //TODO: trigger email selection on native app
-    if ("AppInventor" in window) {
-      setWebString("login");
-    } else {
-      loginUser("test@gmail.com");
-    }
-  };
-
-  let loginUser = async (email) => {
-    //check wether its already a user
-
-    try {
-      let { data, error } = await supabase
-        .from("users")
-        .select()
-        .eq("email", email);
-
-      if (error == null && data.length == 0) {
-        //create new user
-        let { error } = await supabase.from("users").insert({ email: email });
-        if (error == null) {
-          localStorage.setItem("user", email);
-          setLoggedIn(true);
-          navigate("/");
-        } else {
-          toast.error("something went wrong");
-        }
-      } else if (error == null && data[0].email == email) {
-        //login user
-        localStorage.setItem("user", email);
-        setLoggedIn(true);
-        navigate("/");
-      } else {
-        toast.warn("something went wrong");
-      }
-    } catch (e) {
-      toast.warn("something went wrong");
-    }
+  let loginUser = async () => {
+    setLoggedIn(true);
+    navigate("/");
   };
 
   return (
@@ -70,7 +17,7 @@ function GetStarted({ setLoggedIn }) {
 
       <div className="absolute items-end p-8 flex bg-gradient-to-b from-transparent to-black top-0 left-0 h-[100vh] w-[100vw]">
         <motion.div
-          onClick={() => getStarted()}
+          onClick={() => loginUser()}
           whileTap={{
             scale: 1.1,
           }}
